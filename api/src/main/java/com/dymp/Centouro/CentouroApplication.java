93fadf4273731e5f77e9e9215c5debc347f0aeac
DouglasIde <douglasymide@gmail.com>
2024-08-23 17:13:05 -0300

    first commit

---
+++ api/src/main/java/com/dymp/Centouro/CentouroApplication.java
@@ -0,0 +1,13 @@
+package com.dymp.Centouro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentouroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentouroApplication.class, args);
+	}
+
+}
